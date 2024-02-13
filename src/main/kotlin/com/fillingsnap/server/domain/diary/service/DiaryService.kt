acbb91225a26ba3e8fa5fbd1f178d9807d498910
rangjin <46679581+rangjin@users.noreply.github.com>
2024-02-14 03:53:18 +0900

    스토리, 일기 조회 시 소유자 판별 로직 추가

--- src/main/kotlin/com/fillingsnap/server/domain/diary/service/DiaryService.kt
+++ src/main/kotlin/com/fillingsnap/server/domain/diary/service/DiaryService.kt
@@ -3,9 +3,11 @@ package com.fillingsnap.server.domain.diary.service
 import com.fillingsnap.server.domain.diary.dao.DiaryRepository
 import com.fillingsnap.server.domain.diary.dto.DiaryWithStudyDto
 import com.fillingsnap.server.domain.diary.dto.SimpleDiaryDto
+import com.fillingsnap.server.domain.user.domain.User
 import com.fillingsnap.server.global.exception.CustomException
 import com.fillingsnap.server.global.exception.ErrorCode
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 
 @Service
@@ -16,16 +18,25 @@ class DiaryService (
 ) {
 
     fun getDiaryList(): List<SimpleDiaryDto> {
-        return diaryRepository.findAll().map {
+        val user = SecurityContextHolder.getContext().authentication.principal as User
+
+        return diaryRepository.findAllByUser(user).map {
             SimpleDiaryDto(it)
         }
     }
 
     fun getDiaryById(id: Long): DiaryWithStudyDto {
-        return DiaryWithStudyDto(
-            diaryRepository.findByIdOrNull(id)
-                ?: throw CustomException(ErrorCode.DIARY_NOT_FOUND)
-        )
+        val diary = diaryRepository.findByIdOrNull(id)
+            ?: throw CustomException(ErrorCode.DIARY_NOT_FOUND)
+
+        val user = SecurityContextHolder.getContext().authentication.principal as User
+
+        // 해당 일기의 소유자가 아닌 경우
+        if (user.id!! != diary.user.id!!) {
+            throw CustomException(ErrorCode.NOT_YOUR_DIARY)
+        }
+
+        return DiaryWithStudyDto(diary)
     }
 
 }
