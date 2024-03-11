c1cba9676dca469b79010fd98c53f9605ce6d191
rangjin <46679581+rangjin@users.noreply.github.com>
2024-03-11 18:54:23 +0900

    /diary/test 수정

--- src/main/kotlin/com/fillingsnap/server/domain/diary/web/DiaryController.kt
+++ src/main/kotlin/com/fillingsnap/server/domain/diary/web/DiaryController.kt
@@ -4,13 +4,16 @@ import com.fillingsnap.server.domain.diary.dto.DiaryWithStudyDto
 import com.fillingsnap.server.domain.diary.dto.SimpleDiaryDto
 import com.fillingsnap.server.domain.diary.service.DiaryService
 import io.swagger.v3.oas.annotations.Operation
-import org.apache.coyote.Response
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.springframework.http.ResponseEntity
 import org.springframework.messaging.simp.SimpMessageSendingOperations
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.TimeUnit
 
 @RestController
 @RequestMapping("/diary")
@@ -25,7 +28,27 @@ class DiaryController (
     @Operation(summary = "웹소켓 테스트(/queue/channel/{id}로 hello 전송, 삭제 예정)")
     @GetMapping("/test")
     fun test(id: Long) {
-        sendingOperations.convertAndSend("/queue/channel/$id", "hello")
+        val coroutineScope = CoroutineScope(Dispatchers.IO)
+        coroutineScope.launch {
+            val list = arrayOf(
+                "오늘은 카페에 와서 조용",
+                "하게 책을 읽으며 편안",
+                "한 시간을 보냈다. 테이블과",
+                " 의자가 편안해서 계속 앉아",
+                " 있고 싶었고, 커피숍",
+                " 카운터에서는 따뜻한 음료를",
+                " 마시며 창가에 앉아 유리창",
+                "을 바라보았다. 책장에는 다",
+                "양한 책들이 가득했고,",
+                " 이곳에서 시간을 보내는 것이 정말",
+                " 행복했다.\\\\e"
+            )
+
+            for (string in list) {
+                sendingOperations.convertAndSend("/queue/channel/$id", string)
+                TimeUnit.SECONDS.sleep(1)
+            }
+        }
     }
 
     @Operation(summary = "일기 전체 조회")
