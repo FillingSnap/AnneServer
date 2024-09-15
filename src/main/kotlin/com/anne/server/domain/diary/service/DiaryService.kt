8d2c5a94d0af9feeff645af3edd99bfc8e5cb9c9
rangjin <46679581+rangjin@users.noreply.github.com>
2024-09-15 19:27:53 +0900

    스토리 생성과 일기 생성 로직 분리

--- src/main/kotlin/com/anne/server/domain/diary/service/DiaryService.kt
+++ src/main/kotlin/com/anne/server/domain/diary/service/DiaryService.kt
@@ -2,13 +2,11 @@ package com.anne.server.domain.diary.service
 
 import com.anne.server.domain.diary.dao.DiaryRepository
 import com.anne.server.domain.diary.dto.response.DiaryWithStoryResponseDto
-import com.anne.server.domain.diary.dto.request.DiaryGenerateRequestDto
 import com.anne.server.domain.diary.dto.request.DiaryUpdateRequestDto
-import com.anne.server.domain.story.service.StoryService
+import com.anne.server.domain.story.dao.StoryRepository
 import com.anne.server.domain.user.domain.User
 import com.anne.server.global.exception.CustomException
 import com.anne.server.global.exception.ErrorCode
-import com.anne.server.infra.amazon.AwsS3Service
 import com.anne.server.infra.openai.OpenAiService
 import com.anne.server.infra.openai.dto.SseResponseDto
 import com.anne.server.infra.openai.dto.SseStatus
@@ -17,47 +15,20 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
-import org.springframework.web.multipart.MultipartFile
 import reactor.core.publisher.Flux
-import java.awt.image.BufferedImage
-import java.io.ByteArrayOutputStream
-import java.util.*
-import javax.imageio.ImageIO
 
 @Service
-class DiaryService (
+class DiaryService(
 
     private val diaryRepository: DiaryRepository,
 
-    private val storyService: StoryService,
+    private val storyRepository: StoryRepository,
 
-    private val awsS3Service: AwsS3Service,
-
-    private val openAiService: OpenAiService,
+    private val openAiService: OpenAiService
 
 ) {
 
-    fun imageResize(image: String): ByteArray {
-        val inputStream = awsS3Service.getObject(image)
-
-        val sourceImage = ImageIO.read(inputStream)
-        val width = 640
-        val height = 640
-        val resizeImage = BufferedImage(width, height, sourceImage.type)
-        val g = resizeImage.createGraphics()
-        g.drawImage(sourceImage, 0, 0, width, height, null)
-        g.dispose()
-
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        ImageIO.write(resizeImage, "png", byteArrayOutputStream)
-
-        return byteArrayOutputStream.toByteArray()
-    }
-
-    fun generateDiary(imageList: List<MultipartFile>?, request: DiaryGenerateRequestDto, delay: Long): Flux<SseResponseDto> {
-        val textList = request.textList
-        val uuid = request.uuid!!
-
+    fun generateDiary(delay: Long, uuid: String): Flux<SseResponseDto> {
         if (diaryRepository.existsDiaryByUuid(uuid)) {
             return Flux.just(
                 SseResponseDto(
@@ -67,13 +38,16 @@ class DiaryService (
             )
         }
 
-        val storyList = storyService.createStories(imageList, textList, uuid)
-
-        val imageTextList = storyList.map {
-            Pair(Base64.getEncoder().encodeToString(imageResize(it.image)), it.text)
+        if (!storyRepository.existsStoryByUuid(uuid)) {
+            return Flux.just(
+                SseResponseDto(
+                    status = SseStatus.ERROR,
+                    content = ErrorCode.STORY_NOT_FOUND.message
+                )
+            )
         }
 
-        return openAiService.openAi(uuid, imageTextList, delay)
+        return openAiService.openAi(uuid, delay)
     }
 
     fun getDiaryList(pageable: Pageable): Page<DiaryWithStoryResponseDto> {
