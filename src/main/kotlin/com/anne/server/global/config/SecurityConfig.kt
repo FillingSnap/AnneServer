360bd387e3f33dea01dd58d64b5ca9217c4acc4f
rangjin <rangjin2037@gmail.com>
2025-08-18 18:09:12 +0900

    InternalTokenFilter 추가

--- src/main/kotlin/com/anne/server/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/anne/server/global/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package com.anne.server.global.config
 
 import com.anne.server.global.auth.jwt.AuthenticationEntryPoint
 import com.anne.server.global.auth.jwt.AuthenticationFilter
+import com.anne.server.global.filter.InternalTokenFilter
 import jakarta.servlet.DispatcherType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,6 +15,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 class SecurityConfig (
 
     private val authenticationFilter: AuthenticationFilter,
+
+    private val internalTokenFilter: InternalTokenFilter,
 
     private val authenticationEntryPoint: AuthenticationEntryPoint
 
@@ -28,11 +31,12 @@ class SecurityConfig (
             it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         }
         .authorizeHttpRequests {
-            it.requestMatchers("/internal/drain/**", "/user/token/refresh", "/error", "/login/fedCM/**",
+            it.requestMatchers("/internal/**", "/user/token/refresh", "/error", "/login/fedCM/**",
                 "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .dispatcherTypeMatchers(DispatcherType.ASYNC).permitAll()
                 .anyRequest().authenticated()
         }
+        .addFilterBefore(internalTokenFilter, UsernamePasswordAuthenticationFilter::class.java)
         .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
         .exceptionHandling {
             it.authenticationEntryPoint(authenticationEntryPoint)
