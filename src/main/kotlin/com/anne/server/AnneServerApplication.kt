d18994d80c48b0b598b56bc32fad17433655b7e6
rangjin <rangjin2037@gmail.com>
2025-10-09 03:47:32 +0900

    Logger 분리

--- src/main/kotlin/com/anne/server/AnneServerApplication.kt
+++ src/main/kotlin/com/anne/server/AnneServerApplication.kt
@@ -1,6 +1,5 @@
 package com.anne.server
 
-import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -8,8 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @SpringBootApplication
 @EnableScheduling
 class AnneServerApplication
-
-inline fun <reified T> T.logger() = LoggerFactory.getLogger(T::class.java)!!
 
 fun main(args: Array<String>) {
     runApplication<AnneServerApplication>(*args)
